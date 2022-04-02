6fba8319130c3466910aa0302d67efb96492b5f3
asasigure <mkmokymh@gmail.com>
2022-04-02 23:55:00 +0900

    add: ヲタ芸動画検索

--- handler.js
+++ handler.js
@@ -1,15 +1,18 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async (event) => {
+  const { got } = await import("got");
+
+  const response = await got("https://www.googleapis.com/youtube/v3/search", {
+    searchParams: {
+      key: process.env.YOUTUBE_DATA_API_KEY,
+      part: "snippet",
+      q: "ヲタ芸",
+    },
+  }).json();
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(response),
   };
 };
